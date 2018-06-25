e9b7f94fefff54609785655f9bc38692047e6b9f
SARASWANG <2337523303qq.com>
2018-06-25 21:33:18 +0800

    公开静态资源和第三方资源包

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 // 入口程序
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 // 导入路由模块
-var router = require('./router.js')
-
+var router = require('./router.js');
 
 // 配置express-art-template
 app.engine('html', require('express-art-template')); 
+
+// 公开静态资源------??
+app.use('/public', express.static('./public/'))
+// 公开第三方资源包
+app.use('/node_modules', express.static('./node_modules/'))
+
+
 
 // 挂载路由
 app.use(router)
